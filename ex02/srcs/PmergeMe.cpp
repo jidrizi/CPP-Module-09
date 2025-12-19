f4bb7ac87972e84407be24aa46a428fd00afbaed
Juxhin Idrizi <jidrizi@student.42heilbronn.de>
2025-12-19 21:41:01 +0100

    i'm going to need look at something as reference because if i continue to try it alone i won't finish this in time. this isnt working

--- ex02/srcs/PmergeMe.cpp
+++ ex02/srcs/PmergeMe.cpp
@@ -6,75 +6,8 @@
 /*   By: jidrizi <jidrizi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 16:28:49 by jidrizi           #+#    #+#             */
-/*   Updated: 2025/12/18 00:24:11 by jidrizi          ###   ########.fr       */
+/*   Updated: 2025/12/19 21:17:37 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PmergeMe.hpp"
 
-// Constructors
-
-PmergeMe::PmergeMe()
-{
-	std::cout << "\033[32mDefault PmergeMe constructor called\033[0m"
-		<< std::endl;
-}
-
-PmergeMe::PmergeMe(PmergeMe& src) : sortingVector(src.sortingVector), sortingDeque(src.sortingDeque)
-{
-	std::cout << "\033[32mCopy PmergeMe constructor called\033[0m"
-		<< std::endl;
-}
-
-PmergeMe& PmergeMe::operator=(PmergeMe& src)
-{
-	std::cout << "\033[32mCopy PmergeMe assigment operator called\033[0m"
-		<< std::endl;
-	if (this != &src)
-	{
-		this->sortingVector = src.sortingVector;
-		this->sortingDeque = src.sortingDeque;
-	}
-	return (*this);
-}
-
-PmergeMe::~PmergeMe()
-{
-	std::cout << "\033[32mPmergeMe destructor called\033[0m"
-		<< std::endl;
-}
-
-// Functions
-
-void	PmergeMe::getVectorElements(char** argv)
-{
-
-	argv++;
-	std::cout << findArgQuantity(argv) << std::endl;
-	this->sortingVector.reserve(findArgQuantity(argv));
-	while (argv[0])
-	{
-		std::stringstream	elementStr(argv[0]);
-		unsigned int		elementUnsigInt;
-
-		elementStr >> elementUnsigInt;
-		this->sortingVector.push_back(elementUnsigInt);
-
-		argv++;
-	}
-
-	std::cout << "Before: ";
-	for (size_t i = 0; i < this->sortingVector.size(); i++)
-		std::cout << this->sortingVector[i] << " ";
-	std:: cout << std::endl;
-}
-
-// void	PmergeMe::executeVectorSort(int n)
-// {
-// 	std::vector<int>						pair;
-// 	static std::vector<std::vector<int>>	pairHolder;
-
-	
-
-	
-// }
--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,78 +6,15 @@
 /*   By: jidrizi <jidrizi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 16:28:08 by jidrizi           #+#    #+#             */
-/*   Updated: 2025/12/18 00:24:36 by jidrizi          ###   ########.fr       */
+/*   Updated: 2025/12/19 21:39:25 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-int	findArgQuantity(char **argv)
-{
-	int	i = 0;
 
-	for ( ; argv[0]; argv++)
-		i++;
-	return (i);
-}
-
-int	checkDups(char** argv)
-{
-	int current = 0;
-	int next = 1;
-
-	while (argv[current])
-	{
-		while (argv[current + next])
-		{
-			if (strcmp(argv[current], argv[current + next]) == 0)
-				return (EXIT_FAILURE);
-			next++;
-		}
-		next = 1;
-		current++;
-	}
-
-	return (EXIT_SUCCESS);
-}
-
-int	checkArgs(int argc, char** argv)
-{
-	if (argc < 3)
-		return (std::cerr << "Error: not enough arguments.\n", EXIT_FAILURE);
-
-	int	currStr = 1;
-	int	currChar = 0;
-	while (argv[currStr])
-	{
-		while (argv[currStr][currChar])
-		{
-			if (argv[currStr][currChar] 
-					&& (argv[currStr][currChar] < '0' || argv[currStr][currChar] > '9'))
-			{
-				return (std::cerr << "Error: only numbers allowed\n", 
-							EXIT_FAILURE);
-			}
-			currChar++;
-		}
-		currChar = 0;
-		currStr++;
-	}
-
-	if (checkDups(argv) == EXIT_FAILURE)
-		return (std::cerr << "Error: no duplicate allowed\n", EXIT_FAILURE);
-
-	return (EXIT_SUCCESS);
-}
 
 int	main(int argc, char** argv)
 {
-	if (checkArgs(argc, argv) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
 
-	PmergeMe	containerClass;
-	containerClass.getVectorElements(argv);
-	// containerClass.executeVectorSort(2);
-	
-	return (EXIT_SUCCESS);
 }
